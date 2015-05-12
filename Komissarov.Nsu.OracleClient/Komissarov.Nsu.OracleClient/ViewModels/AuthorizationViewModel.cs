2ad458afe648ecf8aa89f692e3561f837a5caef1
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2015-05-12 22:18:40 +0600

    Added stub for main window

--- Komissarov.Nsu.OracleClient/Komissarov.Nsu.OracleClient/ViewModels/AuthorizationViewModel.cs
+++ Komissarov.Nsu.OracleClient/Komissarov.Nsu.OracleClient/ViewModels/AuthorizationViewModel.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using Caliburn.Micro;
+using Komissarov.Nsu.OracleClient.Accessor;
+using Komissarov.Nsu.OracleClient.Views;
 
 namespace Komissarov.Nsu.OracleClient.ViewModels
 {
-	class AuthorizationViewModel : PropertyChangedBase
+	class AuthorizationViewModel : Screen//, IViewAware
 	{
-		private string _userName, _password, _tablespace;
+		private string _dataSource, _userName, _tablespace;
 		private IWindowManager _manager;
+
+		public string DataSource
+		{
+			get
+			{
+				return _dataSource;
+			}
+			set
+			{
+				if ( value == _dataSource )
+					return;
+				_dataSource = value;
+				NotifyOfPropertyChange( ( ) => DataSource );
+			}
+		}
 
 		public string UserName
 		{
@@ -22,26 +40,6 @@ namespace Komissarov.Nsu.OracleClient.ViewModels
 					return;
 				_userName = value;
 				NotifyOfPropertyChange( ( ) => UserName );
-			}
-		}
-
-		public AuthorizationViewModel( IWindowManager winManager )
-		{
-			_manager = winManager;
-		}
-
-		public string Password
-		{
-			get
-			{
-				return _password;
-			}
-			set
-			{
-				if ( value == _password )
-					return;
-				_password = value;
-				NotifyOfPropertyChange( ( ) => Password );
 			}
 		}
 
@@ -60,11 +58,37 @@ namespace Komissarov.Nsu.OracleClient.ViewModels
 			}
 		}
 
+		public AuthorizationViewModel( IWindowManager winManager )
+		{
+			_manager = winManager;
+			DataSource = "10.4.0.119";
+			UserName = "12201";
+		}
+
 		public void LogIn( )
 		{
-			MessageBox.Show( "Alpha version functional is limited" );
-			//log in
-			_manager.ShowWindow( new AuthorizationViewModel( _manager ) );
+			var authorizationView = GetView( ) as AuthorizationView;
+			if ( authorizationView == null )
+			{
+				MessageBox.Show( "Absolutely unexpected fatal error!", "Error" );
+				return;	
+			}
+
+			PasswordBox box = authorizationView.passwordBox;
+
+			OracleAccessor accessor = null;
+
+			try
+			{
+				accessor = new OracleAccessor( _dataSource, _userName, box.Password, _tablespace );
+			}
+			catch ( Exception e )
+			{
+				int i = 5;
+			}
+			
+			
+			_manager.ShowWindow( new MainViewModel( _manager, accessor ) );
 		}
 	}
 }
---
+++ Komissarov.Nsu.OracleClient/Komissarov.Nsu.OracleClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Caliburn.Micro;
+using Komissarov.Nsu.OracleClient.Accessor;
+
+namespace Komissarov.Nsu.OracleClient.ViewModels
+{
+	class MainViewModel : PropertyChangedBase
+	{
+		private IWindowManager _manager;
+		private OracleAccessor _accessor;
+
+		public string Stub
+		{
+			set;
+			get;
+		}
+
+		public MainViewModel( IWindowManager manager, OracleAccessor accessor )
+		{
+			_manager = manager;
+			_accessor = accessor;
+			foreach ( var name in accessor.GetAvaliableTables( ) )
+				Stub += name + "\n";
+		}
+	}
+}
