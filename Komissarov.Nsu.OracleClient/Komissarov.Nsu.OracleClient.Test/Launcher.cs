6c91e56036490bd7ac59350ebc057b6dafc48774
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2015-05-12 15:23:37 +0600

    Created project for oracle client

---
+++ Komissarov.Nsu.OracleClient/Komissarov.Nsu.OracleClient.Test/Launcher.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Komissarov.Nsu.OracleClient
+{
+	class Launcher
+	{
+		public static void Main( )
+		{
+			OracleConnectionStringBuilder builder = new OracleConnectionStringBuilder( );
+
+			builder.DataSource = @"10.4.0.119";
+			builder.Password = @"123456";
+			builder.UserID = @"12201";
+			builder.PersistSecurityInfo = true;
+
+			//using ( OracleAccessor connector = new OracleAccessor( "10.4.0.119", "12201", "123456", "USERS" ) )
+			//{
+			//	foreach ( string table in connector.GetAvaliableTables( ) )
+			//		Console.WriteLine( table );
+			//}
+
+			using ( OracleConnection connection = new OracleConnection( builder.ToString( ) ) )
+			{
+				connection.Open( );
+
+				using ( OracleCommand command = connection.CreateCommand( ) )
+				{
+					command.CommandText = @"select * from all_tables";
+
+					DataTable table = connection.GetSchema( @"foreignKeyColumns" );
+
+					foreach ( DataRow row in table.Rows )
+					{
+						if ( !row["TABLE_NAME"].ToString( ).Equals( "ROGOLEV_GOODS_TAKEN" ) )
+							continue;
+						foreach ( DataColumn column in table.Columns )
+							Console.WriteLine( "{0} = {1}", column.ColumnName, row[column] );
+						Console.WriteLine( );
+					}
+
+					//using ( var dataReader = command.ExecuteReader( ) )
+					//{
+					//	while ( dataReader.Read( ) )
+					//	{
+					//		string str = dataReader.GetString( dataReader.GetOrdinal( "table_name" ) );
+					//		//try
+					//		//{
+					//		//	str += " " + dataReader.GetString( dataReader.GetOrdinal( "tablespace_name" ) );
+					//		//}
+					//		//catch ( InvalidCastException e )
+					//		//{
+					//		//	str += " null";
+					//		//}
+					//		Console.WriteLine( dataReader[0].ToString( ) );
+					//	}
+					//}
+				}
+			}
+			Console.ReadKey( );
+		}
+	}
+}
---
+++ Komissarov.Nsu.OracleClient/Komissarov.Nsu.OracleClient.Test/OracleAccessor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Komissarov.Nsu.OracleClient.Test
+{
+	class OracleAccessor : IDisposable
+	{
+		private OracleConnection _connection;
+		private List<string> _avaliableTables;
+		private string _userId;
+		private string _tablespace;
+
+		public OracleAccessor( string dataSource, string userId, string password, string tablespace = null )
+		{
+			_avaliableTables = null;
+			_userId = userId;
+			_tablespace = tablespace;
+
+			var builder = new OracleConnectionStringBuilder( );
+			builder.DataSource = dataSource;
+			builder.Password = password;
+			builder.UserID = userId;
+			builder.ValidateConnection = true;
+
+			_connection = new OracleConnection( builder.ToString( ) );
+			_connection.Open( );
+		}
+
+		public void ExecuteQuery( string query )
+		{
+			//todo: implement query execution
+
+			//using ( var command = _connection.CreateCommand( ) )
+			//{
+			//	command.CommandText = query;
+
+			//}
+		}
+
+		public List<string> GetAvaliableTables( )
+		{
+			if ( _avaliableTables != null )
+				return _avaliableTables;
+
+			_avaliableTables = new List<string>( );
+
+			using ( var command = _connection.CreateCommand( ) )
+			{
+				command.CommandText = "select * from ALL_TABLES";
+
+				using ( var dataReader = command.ExecuteReader( ) )
+				{
+					var names = from DbDataRecord row in dataReader
+								where row[row.GetOrdinal( "owner" )].ToString( ).Equals( _userId )
+								&& ( _tablespace == null || row[row.GetOrdinal( "tablespace_name" )].ToString( ).Equals( _tablespace ) )
+								orderby row[row.GetOrdinal( "table_name" )]
+								select row[row.GetOrdinal( "table_name" )].ToString( );
+
+					_avaliableTables = names.ToList( );
+				}
+			}
+			return _avaliableTables;
+		}
+
+		public void Dispose( )
+		{
+			_connection.Dispose( );
+		}
+	}
+}
