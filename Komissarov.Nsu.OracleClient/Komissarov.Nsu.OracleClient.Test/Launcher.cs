8549196698e9ebf995e77ea221a82a8f9498bfb7
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2015-05-19 21:33:08 +0600

    added table search

--- Komissarov.Nsu.OracleClient/Komissarov.Nsu.OracleClient.Test/Launcher.cs
+++ Komissarov.Nsu.OracleClient/Komissarov.Nsu.OracleClient.Test/Launcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Windows.Forms;
 using Oracle.ManagedDataAccess.Client;
 using Komissarov.Nsu.OracleClient.Accessor;
 
@@ -22,48 +23,44 @@ namespace Komissarov.Nsu.OracleClient.Test
 			//		Console.WriteLine( table );
 			//}
 
-			//using ( OracleConnection connection = new OracleConnection( builder.ToString( ) ) )
-			//{
-			//	connection.Open( );
+			using ( OracleConnection connection = new OracleConnection( builder.ToString( ) ) )
+			{
+				connection.Open( );
 
-			//	OracleDataReader dataReader;
+				using ( OracleCommand command = connection.CreateCommand( ) )
+				{
+					command.CommandText = @"select * from all_tables";
 
-			//	//using ( OracleCommand command = connection.CreateCommand( ) )
-			//	//{
-			//	//	command.CommandText = @"select * from all_tables";
+					DataTable table = connection.GetSchema( @"columns" );
 
-			//	//	DataTable table = connection.GetSchema( @"foreignKeyColumns" );
+					foreach ( DataRow row in table.Rows )
+					{
+						if ( !row["TABLE_NAME"].ToString( ).Equals( "ROGOLEV_GOODS" ) )
+							continue;
+						foreach ( DataColumn column in table.Columns )
+							Console.WriteLine( "{0} = {1}", column.ColumnName, row[column] );
+						Console.WriteLine( );
+					}
 
-			//	//	//foreach ( DataRow row in table.Rows )
-			//	//	//{
-			//	//	//	if ( !row["TABLE_NAME"].ToString( ).Equals( "ROGOLEV_GOODS_TAKEN" ) )
-			//	//	//		continue;
-			//	//	//	foreach ( DataColumn column in table.Columns )
-			//	//	//		Console.WriteLine( "{0} = {1}", column.ColumnName, row[column] );
-			//	//	//	Console.WriteLine( );
-			//	//	//}
+					//using ( var dataReader = command.ExecuteReader( ) )
+					//{
+					//	while ( dataReader.Read( ) )
+					//	{
+					//		string str = dataReader.GetString( dataReader.GetOrdinal( "table_name" ) );
+					//		//try
+					//		//{
+					//		//	str += " " + dataReader.GetString( dataReader.GetOrdinal( "tablespace_name" ) );
+					//		//}
+					//		//catch ( InvalidCastException e )
+					//		//{
+					//		//	str += " null";
+					//		//}
+					//		Console.WriteLine( dataReader[0].ToString( ) );
+					//	}
+					//}
+				}
 
-			//	//	dataReader = command.ExecuteReader( );
-
-			//	//	//using ( var dataReader = command.ExecuteReader( ) )
-			//	//	//{
-			//	//	//	while ( dataReader.Read( ) )
-			//	//	//	{
-			//	//	//		string str = dataReader.GetString( dataReader.GetOrdinal( "table_name" ) );
-			//	//	//		//try
-			//	//	//		//{
-			//	//	//		//	str += " " + dataReader.GetString( dataReader.GetOrdinal( "tablespace_name" ) );
-			//	//	//		//}
-			//	//	//		//catch ( InvalidCastException e )
-			//	//	//		//{
-			//	//	//		//	str += " null";
-			//	//	//		//}
-			//	//	//		Console.WriteLine( dataReader[0].ToString( ) );
-			//	//	//	}
-			//	//	//}
-			//	//}
-
-
+			}
 
 
 			//	using ( dataReader )
@@ -84,29 +81,6 @@ namespace Komissarov.Nsu.OracleClient.Test
 			//	}
 
 			//}
-
-			using ( OracleAccessor connector = new OracleAccessor( "10.4.0.119", "12201", "123456" ) )
-			{
-				var dataReader = connector.ExecuteQuery( @"select * from all_tables" );
-
-				using ( dataReader )
-				{
-					while ( dataReader.Read( ) )
-					{
-						string str = dataReader.GetString( dataReader.GetOrdinal( "table_name" ) );
-						//try
-						//{
-						//	str += " " + dataReader.GetString( dataReader.GetOrdinal( "tablespace_name" ) );
-						//}
-						//catch ( InvalidCastException e )
-						//{
-						//	str += " null";
-						//}
-						//Console.WriteLine( dataReader[1].ToString( ) );
-						Console.WriteLine( str );
-					}
-				}
-			}
 
 			Console.ReadKey( );
 		}
