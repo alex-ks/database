8dc60da1c1034b2214e61e3ac60978f47b8226e6
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2015-05-26 15:27:07 +0600

    Added table editor data sources

--- Komissarov.Nsu.OracleClient/Komissarov.Nsu.OracleClient/ViewModels/Tabs/TableEditorViewModel.cs
+++ Komissarov.Nsu.OracleClient/Komissarov.Nsu.OracleClient/ViewModels/Tabs/TableEditorViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using Caliburn.Micro;
 using Komissarov.Nsu.OracleClient.Models;
+using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,12 @@ namespace Komissarov.Nsu.OracleClient.ViewModels.Tabs
 	{
 		private IAccessProvider _provider;
 		private string _tableName;
+
+		public bool Created
+		{
+			private set;
+			get;
+		}
 
 		public ObservableCollection<Column> Columns
 		{
@@ -28,9 +36,55 @@ namespace Komissarov.Nsu.OracleClient.ViewModels.Tabs
 			}
 		}
 
+		public Column SelectedColumn
+		{
+			set;
+			get;
+		}
+
+		public List<string> AvaliableTables
+		{
+			get
+			{
+				return _provider.Accessor.GetAvaliableTables( );
+			}
+		}
+
+		public List<string> AvaliableColumnsToRef
+		{
+			get
+			{
+				if ( SelectedColumn.SourceTable == null )
+					return null;
+
+				try
+				{
+					using ( var dataReader = _provider.Accessor.GetTableInfo( SelectedColumn.SourceTable ) )
+					{
+						var names = from DbDataRecord row in dataReader
+									where row[row.GetOrdinal( "primary_key" )].Equals( "P" )
+									orderby row[row.GetOrdinal( "column_name" )]
+									select row[row.GetOrdinal( "column_name" )].ToString( );
+
+						return names.ToList( );
+					}
+				}
+				catch ( OracleException )
+				{
+					return null;
+				}
+
+				//return null;
+			}
+		}
+
 		public TableEditorViewModel( IAccessProvider provider, string tableName )
 		{
 			_provider = provider;
+			if ( tableName == null )
+				Created = true;
+			else
+				Created = false;
 
 			//stub
 			Columns = new BindableCollection<Column>( );
@@ -39,7 +93,7 @@ namespace Komissarov.Nsu.OracleClient.ViewModels.Tabs
 				{
 					Name = i.ToString( ),
 					Type = i.ToString( ),
-					Nullable = i % 2 == 0
+					ForeignKey = i % 2 == 0
 				} );
 		}
 
